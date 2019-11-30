47a93ac22ccc69fff769b2408873bd5c7731d9a0
Nano Miratus <nanomiratus@gmail.com>
2019-11-30 20:11:13 +0100

    add service worker registering for pwa eligibility (#114)

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./scenes/App";
 import './storage';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorker.register();
