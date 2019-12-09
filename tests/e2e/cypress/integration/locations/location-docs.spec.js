26bcdb7e3cce5a54b109927aaabe755e67666bcd
Anton Rusinov <rusinovantondev@gmail.com>
2019-12-09 21:58:00 +0200

    test(cy): update tests

--- tests/e2e/cypress/integration/locations/location-docs.spec.js
+++ tests/e2e/cypress/integration/locations/location-docs.spec.js
@@ -1,48 +1,56 @@
 /// <reference types="Cypress" />
 
 context(`Location | Docs Pages`, () => {
-    it('cy.hash() - get the current URL hash', () => {
-        cy.fixture('docspaths').then((object) => {
-            object.domains.forEach(function(domain) {
-                const path = domain.path
-                cy.visit(`/${path}/${domain.methods.prototype[0]}`)
+  it('cy.hash() - get the current URL hash', () => {
+    cy.fixture('docspaths').then(object => {
+      object.domains.forEach(function(domain) {
+        const path = domain.path;
+        if (!domain.methods || !domain.methods.prototype) return;
+        cy.visit(`/${path}/${domain.methods.prototype[0]}`);
 
-                // https://on.cypress.io/hash
-                cy.hash().should('be.empty')
-            })
-        })
-    })
-              
-    it('cy.location() - get window.location', () => {
-        cy.fixture('docspaths').then((object) => {
-            object.domains.forEach(function(domain) {
-                const path = domain.path
-                cy.visit(`/${path}/${domain.methods.prototype[0]}`)
-                // https://on.cypress.io/location
-                cy.location().should((location) => {
-                    expect(location.hash).to.be.empty
-                    expect(location.href).to.eq(`http://localhost:3000${path}/${domain.methods.prototype[0]}`)
-                    expect(location.host).to.eq('localhost:3000')
-                    expect(location.hostname).to.eq('localhost')
-                    expect(location.origin).to.eq('http://localhost:3000')
-                    expect(location.pathname).to.eq(`${path}/${domain.methods.prototype[0]}`)
-                    expect(location.port).to.eq('3000')
-                    expect(location.search).to.be.empty
-                })
+        // https://on.cypress.io/hash
+        cy.hash().should('be.empty');
+      });
+    });
+  });
 
-            })
-        })
-    })
-              
-    it('cy.url() - get the current URL', () => {
-        cy.fixture('docspaths').then((object) => {
-            object.domains.forEach(function(domain) {
-                const path = domain.path;
+  it('cy.location() - get window.location', () => {
+    cy.fixture('docspaths').then(object => {
+      object.domains.forEach(function(domain) {
+        const path = domain.path;
+        if (!domain.methods || !domain.methods.prototype) return;
+        cy.visit(`/${path}/${domain.methods.prototype[0]}`);
+        // https://on.cypress.io/location
+        cy.location().should(location => {
+          expect(location.hash).to.be.empty;
+          expect(location.href).to.eq(
+            `http://localhost:3000${path}/${domain.methods.prototype[0]}`,
+          );
+          expect(location.host).to.eq('localhost:3000');
+          expect(location.hostname).to.eq('localhost');
+          expect(location.origin).to.eq('http://localhost:3000');
+          expect(location.pathname).to.eq(
+            `${path}/${domain.methods.prototype[0]}`,
+          );
+          expect(location.port).to.eq('3000');
+          expect(location.search).to.be.empty;
+        });
+      });
+    });
+  });
 
-                cy.visit(`/${path}/${domain.methods.prototype[0]}`)
-                // https://on.cypress.io/url
-                cy.url().should('eq', `http://localhost:3000${path}/${domain.methods.prototype[0]}`)
-            })
-        })
-    })
-})
+  it('cy.url() - get the current URL', () => {
+    cy.fixture('docspaths').then(object => {
+      object.domains.forEach(function(domain) {
+        const path = domain.path;
+        if (!domain.methods || !domain.methods.prototype) return;
+        cy.visit(`/${path}/${domain.methods.prototype[0]}`);
+        // https://on.cypress.io/url
+        cy.url().should(
+          'eq',
+          `http://localhost:3000${path}/${domain.methods.prototype[0]}`,
+        );
+      });
+    });
+  });
+});
