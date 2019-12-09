f36e6c89dabbdbe2e0c7c751b454b35c43f17482
Anton Rusinov <rusinovantondev@gmail.com>
2019-12-09 23:57:56 +0200

    fix: gitignore generic files

--- build-search-index.js
+++ build-search-index.js
@@ -10,5 +10,10 @@ const searchIndex = populateDocsIndexWithFileData(normalizedDocsIndex);
 
 const file = JSON.stringify(searchIndex, null, 2);
 
-const filepath = path.resolve(__dirname, './public/docs/search-index.json');
+const filepath = path.resolve(
+  __dirname,
+  process.env.NODE_ENV === 'production' ? 'build' : 'public',
+  'docs/search-index.json',
+);
+console.log('Building index for path: ', filepath);
 fs.writeFileSync(filepath, file, { flag: 'w' });
