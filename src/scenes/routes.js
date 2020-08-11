5de6500794e0af54341b71e3ec2f73552ee7d952
ali <a.hosseini12@outlook.com>
2020-08-11 21:08:36 +0430

    feat: routes improved

--- src/scenes/routes.js
+++ src/scenes/routes.js
@@ -1,4 +1,6 @@
 export const HOME = '/';
-export const DOCS = '/docs/:domain/:method';
 export const ABOUT = '/about';
 export const PRIVACY_POLICY = '/privacy-policy';
+export const DOCS_DEFAULT_ROUTE = '/docs';
+export const DOCS_DOMAIN = '/:domain';
+export const DOCS_METHOD = '/:method';
